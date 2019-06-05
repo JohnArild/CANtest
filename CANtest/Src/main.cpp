455800fba56b6ab3352cd500255c36c0bb19ab1d
John Arild Lolland <JohnArild@protonmail.com>
2019-06-05 23:54:42 +0200

    Changed SYSCLK to 48MHz Added README.md and LICENSE Added vscode workspace files

--- CANtest/Src/main.cpp
+++ CANtest/Src/main.cpp
@@ -5,13 +5,13 @@ void waitLoop(int loops);
 int main(void)
 {
 	// Clock setup. External 8MHz.
-	FLASH->ACR |= FLASH_ACR_LATENCY_2; //Set Flash Latency to 2 for 72MHz
+	FLASH->ACR |= FLASH_ACR_LATENCY_1; //Set Flash Latency to 1 for 48MHz  2 for 72MHz
 	FLASH->ACR |= FLASH_ACR_PRFTBE; //Enable prefetch buffer
 	RCC->APB2ENR |= RCC_APB2ENR_IOPDEN; // HSE is on PD0 and PD1
 	AFIO->MAPR &= ~(AFIO_MAPR_PD01_REMAP); // Set PD0 as OSC_In and PD1 as OSC_out
 	RCC->CR |= RCC_CR_HSEON; // Turn in external clock input
 	while(!((RCC->CR & RCC_CR_HSERDY)== RCC_CR_HSERDY)){;}
-	RCC->CFGR |= RCC_CFGR_PLLMULL9; // Set PLL to x9
+	RCC->CFGR |= RCC_CFGR_PLLMULL6; // Set PLL to x6 (set to 9 for 72MHz)
 	RCC->CFGR |= RCC_CFGR_PPRE1_DIV2; // Set APB1 devider to /2
 	RCC->CFGR |= RCC_CFGR_PLLSRC; // Set PREDIV1 as source for PLL
 	RCC->CR |= RCC_CR_PLLON; // Turn on PLL
@@ -37,7 +37,7 @@ void waitLoop(int loops)
 {
 	for(int i = 0; i < loops; i++)
 	{
-		for(int j = 0; j < 0xFE; j++)
+		for(uint32_t j = 0; j < 0xFFF; j++)
 		{
 			;
 		}
---
+++ README.md
@@ -0,0 +1,6 @@
+# CANtest
+This project is created to test CAN functionality on BluePill
+
+Testboard is STM32F103C8T6 based BluePill.
+
+Released under BSD License, see file named LICENSE
